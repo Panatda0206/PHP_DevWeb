25a1fb4c2a274b575cdff822eaee69f8ec8aacee
Panatda Phanthavong <panatda.phanthavong@iut-dion.u-bourgogne.fr>
2025-11-24 11:50:37 +0100

    Ajouter home.php et template.php

--- TP_Mihoyo/index.php
+++ TP_Mihoyo/index.php
@@ -1,11 +1,21 @@
 <?php
-require_once("Helpers\Psr4AutoloaderClass.php");
+declare(strict_types=1);
+require_once __DIR__ . '/Helpers/Psr4AutoloaderClass.php';
+require_once ('/Vendor/Plates/src/Engine.php');
 
-private Psr4AutoloaderClass $loader;
+use League\Plates\Engine;
+$loader = new Helpers\Psr4AutoloaderClass();
 
 $loader->register();
 
-$loader->addNamespace('\Helpers', '\Helpers');
+$loader->addNamespace('\Helpers', '/Helpers');
+$loader->addNamespace('\League\Plates', '/Vendor/Plates/src');
 
+$templates = new Engine(__DIR__ . '/Views');
+
+// On génère la vue "home"
+echo $templates->render('home', [
+    'gameName' => 'Genshin Impact'
+]);
 
 ?>
